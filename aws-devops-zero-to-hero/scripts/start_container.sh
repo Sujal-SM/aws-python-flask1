9956db467670b3b56e99712b4220f1ca75082dfd
Sujal Malhotra <malhotrasujal98@gmail.com>
2025-07-22 19:24:04 +0530

    new-commit-for code deploy

--- aws-devops-zero-to-hero/scripts/start_container.sh
+++ aws-devops-zero-to-hero/scripts/start_container.sh
@@ -2,7 +2,6 @@
 set -e
 
 # Pull the Docker image from Docker Hub
-docker pull abhishekf5/simple-python-flask-app
-
+docker pull sujalll11/simple-python-app
 # Run the Docker image as a container
-docker run -d -p 5000:5000 abhishekf5/simple-python-flask-app
+docker run -d -p 5000:5000 sujalll11/simple-python-app
